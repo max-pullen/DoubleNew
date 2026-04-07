7af722ece5cccc1ff522a73282a3b2864a273667
Max Pullen <maxojpullen@gmail.com>
2026-04-07 10:46:14 +0000

    Challenge code added to main

--- main/src/main/java/Test.java
+++ main/src/main/java/Test.java
@@ -1,2 +1,11 @@
 public class Test {
+    public class Main {
+        public static void main(String[] args) {
+            Greeter greeter = new Greeter();
+            Calculator calculator = new Calculator();
+
+            greeter.greet("World");
+            System.out.println(calculator.add(3, 5));
+        }
+    }
 }
